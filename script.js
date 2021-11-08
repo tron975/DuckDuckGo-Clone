8410d247583f814dc0745af91287d9bdefa232b0
tron975 <79353357+tron975@users.noreply.github.com>
2021-11-07 21:09:26 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const searchText = document.getElementById('search-text')
+
+searchText.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      // Trigger the button element with a click
+      document.querySelector(".search-icon").click();
+    }
+  });
+
+
+const expandArrow = document.querySelector('.expand-card')
+const card = document.querySelector('.card')
+
+function expand() {
+    if (card.style.height !== 'auto') {
+        card.style.height = 'auto'
+        expandArrow.style.transform="rotate(180deg)"
+        expandArrow.style.borderRadius="0"
+    }
+    else {
+        card.style.height = '20rem'
+        expandArrow.style.transform="rotate(0deg)"
+    }
+}
